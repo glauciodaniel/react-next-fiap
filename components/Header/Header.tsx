48e8d8229639f4a09a01efe8440f2ca0c1c3fe89
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-10 16:07:05 -0300

    Deploy

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,6 +14,7 @@ import { css} from '@emotion/css';
 import styled from '@emotion/styled';
 import Link from 'next/link';
 
+//styled-components;
 const HeaderPage = styled.header`
 width: 100%;
 height: 100px;
@@ -21,15 +22,62 @@ background: rgb(247, 243, 232);
 display: flex;
 align-items: center;
 justify-content: space-between;
+& > div:first-child, 
+& > div:last-child { 
+  margin: 0 5%;
+}
+`;
+
+const NavMenu = styled.nav`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  & a:hover {
+    font-weight: bold;
+  }
+  & li {
+    margin-top: 4px;
+    border-bottom: 4px solid transparent;
+    padding-bottom: 20px;
+  }
+  & li:hover {
+    border-bottom: double 4px orange;
+  }  
 `;
 
 
-export default function Header() {
+const InputMenu = styled.input`
+  border: none;
+  border-radius: 10px;
+  padding: 2px 10px;
+  height: 30px;
+`;
+
+const ButtonMenu = styled.button`
+  border: none;
+  border-radius: 10px;
+  height: 30px;
+  margin-left: 5px;
+  color: #333;
+  background: linear-gradient(150deg, #ffeb84 0%, #ffd53e 100%);
+  box-shadow: 0px 2px 5px -1px rgb(59, 59, 59, 0.5);
+`;
+
+//Input
+
+//Button
+
+type HeaderProps = {
+  pesquisar ?: string;
+  menu?: Array<string>;
+}
+export default function Header(props:HeaderProps) {
   return (
     //<header className={styles['menu-site']}>
     <HeaderPage>
         <div>Logotipo</div> 
-        <nav>
+        <NavMenu>
           <ul className={ css`
           margin:0;
           padding:0;
@@ -49,8 +97,11 @@ export default function Header() {
             <li><Link href="#">Blog</Link></li>
             <li><Link href="#">Contato</Link></li>
           </ul>
-        </nav> 
-        <div>Caixa de Busca</div> 
+        </NavMenu> 
+        <div>
+          <InputMenu type="text" placeholder="Digite sua busca" />
+          <ButtonMenu>{props.pesquisar?props.pesquisar:"Search"}</ButtonMenu>
+          </div> 
     </HeaderPage>
   )
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 export default function LoginPage() {
   return (
     <>
-    <Header />
+    <Header pesquisar="Buscar no site" />
     <h1>Seja bem vindo ao React</h1>
     </>
   )
